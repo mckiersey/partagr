a480a9cf1120ce496b08cac22d3e350012dee02a
Sean McKiernan <seanmckiernan01@gmail.com>
2021-07-25 16:39:30 +0200

    removing server name as it may not be needed

--- public/ProductionMode.js
+++ public/ProductionMode.js
@@ -5,7 +5,7 @@ if (ProductionMode == 'DEV') {
     var server = 'http://localhost'
     console.log('Production mode = ', server)
 } else {
-    var server = 'partagr.com'
+    var server = ''
     console.log('Production mode = ', server)
 }
 
