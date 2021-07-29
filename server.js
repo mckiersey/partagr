ded77eceb87871a3cfd483a222b41c3c122cfc92
Sean McKiernan <seanmckiernan01@gmail.com>
2021-07-29 09:16:38 +0200

    adding visibility in video insertion logic

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+//const port = 80;
 const path = require('path');
 const routes = require('./routes.js');
 const bodyParser = require('body-parser')
