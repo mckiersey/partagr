e29435888fc7837f30173362d569269293992388
Seán McKiernan <Sean@Seans-MacBook-Air.local>
2022-01-09 14:22:16 +0000

    add back in Prod mode file

---
+++ public/ProductionMode.js
@@ -0,0 +1,13 @@
+var ProductionMode = 'DEV'
+//var ProductionMode = 'PROD'
+
+if (ProductionMode == 'DEV') {
+    var server = 'http://localhost:5000'
+    console.log('Production mode = ', server)
+} else {
+    var server = ''
+    console.log('Production mode = ', server)
+}
+
+
+
