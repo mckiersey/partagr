eab6427a2ed7afca339acf925521a34564640825
Sean <seanmckiernan01@gmail.com>
2022-05-23 19:46:28 +0100

    disambiguate profile picture names

--- public/profile.js
+++ public/profile.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
 
 
     
-    $(document).on('click', '#MyProfilePicture', function () {
+    $(document).on('click', '#LoggedInProfilePicture', function () {
         var CookieToken = getCookieValue('USER_SESSION_TOKEN')
  
         console.log(server + '/MyProfile')
