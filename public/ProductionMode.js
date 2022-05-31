4f726ca77c2706814a0e692dc00f49bbe6a9824c
Sean <seanmckiernan01@gmail.com>
2022-05-31 23:03:49 +0100

    Test production mode utility

--- public/ProductionMode.js
+++ public/ProductionMode.js
@@ -1,13 +1,14 @@
-//var ProductionMode = 'DEV'
-var ProductionMode = 'PROD'
+server = "";
+/*
+var ProductionMode = "DEV";
+//var ProductionMode = 'PROD'
 
-if (ProductionMode == 'DEV') {
-    var server = 'http://localhost:5000'
-    console.log('Production mode = ', server)
+if (ProductionMode == "DEV") {
+  var server = "http://localhost:5000";
+  console.log("Production mode = ", server);
 } else {
-    var server = ''
-    console.log('Production mode = ', server)
+  var server = "";
+  console.log("Production mode = PROD");
 }
 
-
-
+*/
