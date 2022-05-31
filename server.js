4cd936f80449fa4286aee4b6e1949716d2b9cdb9
Sean <seanmckiernan01@gmail.com>
2022-05-31 09:58:27 +0100

    migration to html: server adjustments

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
-const path = require('path');
-const routes = require('./routes.js');
-const bodyParser = require('body-parser')
-const cors = require('cors'); // From bookshelf
-cookieParser = require('cookie-parser');
-
-
-
+const path = require("path");
+const routes = require("./routes.js");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // From bookshelf
+cookieParser = require("cookie-parser");
 
 // middlewear
-// Setting the view engine
-app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("public"));
 
 // Add body parser for routes.js file
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 //start the server
 const server = app.listen(port, (error) => {
-    if (error) return console.log(`** SERVER ERROR: ${error}`);
+  if (error) return console.log(`** SERVER ERROR: ${error}`);
 
-    console.log(`Server is running on port: ${server.address().port}`);
-
+  console.log(`Server is running on port: ${server.address().port}`);
 });
 
-routes(app)
+routes(app);
